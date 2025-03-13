010f7d702e64e8233b7ffc23da6ff967cbf9a26f
jhuang67 <huang.jol@northeastern.edu>
2025-03-12 20:55:13 -0400

    fixx???????

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -2,19 +2,6 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Lab1 from "./Lab1";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
-<<<<<<< Updated upstream
-
-export default function Labs() {
-    return (
-        <div>
-            <h1>Labs</h1>
-            <Lab1 />
-            <Lab2 />
-            <Lab3 />
-        </div>
-    );
-}
-=======
 import TOC from "./TOC";
 import Lab4 from "./Lab4";
 import store from "./store";
@@ -37,4 +24,3 @@ export default function Labs() {
     </Provider>
   );
 }
->>>>>>> Stashed changes
