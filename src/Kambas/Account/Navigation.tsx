3df1481b269e1fd57f09e6420873ef20ebee2743
jhuang67 <huang.jol@northeastern.edu>
2025-04-17 03:27:24 -0400

    h

--- src/Kambas/Account/Navigation.tsx
+++ src/Kambas/Account/Navigation.tsx
@@ -1,8 +1,10 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const { pathname } = useLocation();
+  const active = (path: string) => (pathname.includes(path) ? "active" : "");
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
 
   return (
@@ -24,6 +26,15 @@ export default function AccountNavigation() {
           <NavLink to="/Kambas/Account/Profile">Profile</NavLink>
           <br />
         </>
+      )}
+      {currentUser && currentUser.role === "ADMIN" && (
+        <NavLink
+          to={`/Kambas/Account/Users`}
+          className={`list-group-item ${active("Users")}`}
+        >
+          {" "}
+          Users{" "}
+        </NavLink>
       )}
     </div>
   );
