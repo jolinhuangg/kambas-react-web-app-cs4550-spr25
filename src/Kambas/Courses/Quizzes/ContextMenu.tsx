cbd6f377d5b3832cfa3e6165855d6b4f65300bbe
olivial215 <li.ol@northeastern.edu>
2025-04-20 23:10:59 -0400

    context menu

---
+++ src/Kambas/Courses/Quizzes/ContextMenu.tsx
@@ -0,0 +1,43 @@
+import { Dropdown} from "react-bootstrap";
+
+import { BiBlock } from "react-icons/bi";
+import { forwardRef } from "react";
+import { BsThreeDotsVertical } from "react-icons/bs";
+
+
+const CustomToggle = forwardRef(({ onClick }, ref) => (
+    <div
+      ref={ref}
+      onClick={(e) => {
+        e.preventDefault();
+        onClick?.(e);
+      }}
+      style={{ 
+        cursor: "pointer",
+        border: "1px solid lightgray", // Bootstrap border color
+        background: "#F0F0F0",
+        borderRadius: "3px",   // Slightly rounded
+        padding: "4px 5px",
+    }}
+      
+    >
+      <BsThreeDotsVertical size={15} />
+    </div>
+  ));
+
+
+  export default function ContextMenu() {
+    return (
+    <Dropdown>
+    <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-toggle" />
+
+        <Dropdown.Menu>
+        <Dropdown.Item>Edit</Dropdown.Item> {/** navigate to QuizDetails */}
+        <Dropdown.Item>Delete</Dropdown.Item> 
+        <Dropdown.Item>Publish</Dropdown.Item> {/** Option becomes Unpublish */}
+        <Dropdown.Item>Copy</Dropdown.Item> {/** copy to another course (optional) */}
+        <Dropdown.Item>Sort</Dropdown.Item> {/** csort by name, due date, available date (optional) */}
+        </Dropdown.Menu>
+    </Dropdown>
+    )
+  }
