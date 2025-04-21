bd3b97e9796a34d17eb839ba84cf07d65c0c4a57
olivial215 <li.ol@northeastern.edu>
2025-04-20 23:30:53 -0400

    details tab

--- src/Kambas/Courses/Quizzes/DetailsEditor.tsx
+++ src/Kambas/Courses/Quizzes/DetailsEditor.tsx
@@ -1,9 +1,60 @@
+import { Container, Dropdown, Nav } from "react-bootstrap";
+import { useParams, Outlet, NavLink } from "react-router";
+import { useEffect, useState } from "react";
+import { BiBlock } from "react-icons/bi";
+
+import ContextMenu from "./ContextMenu";
+
 export default function DetailsEditor() {
+    const { qid, cid } = useParams();
+    const [quiz, setQuiz] = useState<any>(null);
+
+    const tabStyle = {
+        padding: "8px 20px",
+        marginRight: "4px",
+        textDecoration: "none",
+        color: "#000",
+        fontWeight: 500,
+      };
+    
+      const activeTabStyle = {
+        ...tabStyle,
+        backgroundColor: "#ffffff",
+        border: "1px solid #ccc",
+        borderBottom: "2px solid #ffffff",
+        color: "black"
+      };
+
     return (
-      <div className="p-4">
-        <h2>Edit Quiz Details</h2>
-        {/* TODO: Add tabs, form inputs, save/cancel buttons */}
+    <Container>
+      <div className="mt-4 d-flex justify-content-end align-items-center gap-3">
+        <div>  Points {quiz?.points ?? 0} </div>
+        <div className="text-secondary d-flex align-items-center gap-1">  <BiBlock size={20}/> Not Published </div>
+        <ContextMenu/>
       </div>
+
+      <hr/>
+
+      <Container className="mt-4">
+      <div style={{ display: "flex", borderBottom: ".5px solid #ccc" }}>
+        <NavLink
+          to={`/Kambas/Courses/${cid}/Quizzes/${qid}/edit/details`}
+          style= {activeTabStyle }
+        >
+          Details
+        </NavLink>
+        <NavLink
+          to={`/Kambas/Courses/${cid}/Quizzes/${qid}/edit/questions`}
+          style={({ isActive }) => (isActive ? activeTabStyle : tabStyle)}
+        >
+          Questions
+        </NavLink>
+      </div>
+      <hr className="mt-0" />
+      <Outlet />
+    </Container>
+
+    </Container>
     );
   }
   
